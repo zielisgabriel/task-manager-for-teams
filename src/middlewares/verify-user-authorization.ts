68a7aa9abdcca9962fb21e5cf6ef4f27d731fbf9
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-28 12:00:32 -0300

    verify user authorization middleware added

---
+++ src/middlewares/verify-user-authorization.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError";
+
+export function verifyUserAuthorization(role: string[]){
+    return (req: Request, res: Response, next: NextFunction) => {
+        if(!role.includes(req.user.role)){
+            throw new AppError('Não autorizado', 401)
+        }
+
+        next()
+    }
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { UsersController } from '../controllers/users-controller'
 import { ensureAuthentication } from '../middlewares/ensure-authentication'
+import { verifyUserAuthorization } from '../middlewares/verify-user-authorization'
 
 const usersRoutes = Router()
 const usersController = new UsersController()
 
-usersRoutes.get('/', ensureAuthentication, usersController.index)
+usersRoutes.get('/', ensureAuthentication, verifyUserAuthorization(['ADMIN']), usersController.index)
 usersRoutes.post('/', usersController.create)
 
 export { usersRoutes }
